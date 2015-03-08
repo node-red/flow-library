bbbed5f4579fc1271d01e9db810292021cad9a20
Nick O'Leary <nick.oleary@gmail.com>
2015-03-08 22:32:44 +0000

    Add backup

---
+++ backup.js
@@ -0,0 +1,11 @@
+
+var settings = require("./settings");
+var mongojs = require('mongojs');
+
+var db = mongojs(settings.mongo.url,["flows","users","tags"]);
+db.flows.find({},function(err,flows) {
+    console.log(JSON.stringify(flows));
+    db.close();
+});
+
+

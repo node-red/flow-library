303cf1fe9f9b819e719a4f28fbfa990f71bb675a
Nick O'Leary <nick.oleary@gmail.com>
2016-01-11 10:07:17 +0000

    Save updated_at as time.modified

--- migrate.js
+++ migrate.js
@@ -3,39 +3,29 @@ var settings = require("./settings");
 var mongojs = require('mongojs');
 
 var db = mongojs(settings.mongo.url,["flows","users","tags"]);
-db.flows.find({},function(err,flows) {
+db.flows.find({updated_at:{$gt:1452000000000}},function(err,flows) {
         flows.forEach(function(data) {
-                if (data.history) {
-                    delete data.history;
-                    data.owner = {
-                        login: data.owner.login,
-                        avatar_url: data.owner.avatar_url
-                    }
-                    delete data.rateLimit;
-                    data.type = "flow";
-                }
-                
+            console.log(data._id,data.updated_at,data.time.modified);
+            data.updated_at = data.time.modified;
         });
-        
-        
+
+
         function saveFlow() {
             console.log(flows.length+" remaining");
             if (flows.length > 0) {
                 var flow = flows.splice(0,1)[0];
-                
-                
+
+
                 db.flows.save(flow,function(err,oth) {
                     if (err) {
                         console.log(err);
                     } else {
                         saveFlow();
                     }
-                    
+
                 });
             }
         }
         saveFlow();
-        
+
 });
-
-

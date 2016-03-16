d10a4197e467e2d2106a7e545857f4d9e394f9b6
Nick O'Leary <nick.oleary@gmail.com>
2016-03-16 17:09:38 +0000

    Add prune-missing-readme script

---
+++ prune-missing-readme.js
@@ -0,0 +1,28 @@
+// One off script to remove entries that are missing their README files
+
+var settings = require("./settings");
+var npmNodes = require("./lib/nodes");
+var npmModules = require("./lib/modules");
+var db = require('./lib/db');
+var when = require("when");
+
+db.flows.find({$query:{readme:{$regex:/^ERROR: No README data found!/}}},{_id:1}).toArray(function(err,docs) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    var promises = [];
+    docs.forEach(function(r) {
+        promises.push(npmNodes.remove(r._id));
+    });
+    when.settle(promises).then(function(results) {
+        results.forEach(function(res) {
+            if (res.state === 'rejected') {
+                console.log("Failed:",res.reason);
+            } else if (res.value) {
+                console.log("Deleted:",res.value);
+            }
+        });
+        db.close();
+    })
+});

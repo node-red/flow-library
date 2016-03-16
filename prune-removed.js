d4f860344367bf0a0dfe0cbf0f9d22a6e0161fdd
Nick O'Leary <nick.oleary@gmail.com>
2016-03-16 17:02:33 +0000

    Add prune script to remove old modules

---
+++ prune-removed.js
@@ -0,0 +1,15 @@
+var settings = require("./settings");
+var npmNodes = require("./lib/nodes");
+var npmModules = require("./lib/modules");
+
+npmModules.pruneRemoved().then(function(results) {
+    results.forEach(function(res) {
+        if (res.state === 'rejected') {
+            console.log("Failed:",res.reason);
+        } else if (res.value) {
+            console.log("Updated:",res.value);
+        }
+    });
+
+    npmNodes.close();
+});

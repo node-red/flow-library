32a82177e730cecec499fe16f452ea0cd1fea3e1
mblackstock <mikeblackstock@gmail.com>
2018-03-03 14:56:41 -0800

    polish, fix ajax error response

--- routes/index.js
+++ routes/index.js
@@ -14,17 +14,16 @@ app.get("/", function (req, res) {
     query.page = Number(query.page) || 1;
     query.view = "counts";
     context.query = query;
+    context.prevPage = query.page - 1;
 
     viewster.getForQuery(query).then(function (result) {
         context.count = result.count || 0;
         context.total = result.total || 0;
-        context.prevPage = query.page - 1;
         context.nextPage = (context.count - viewster.DEFAULT_PER_PAGE * query.page) > 0 ? query.page+ 1 : 0;
         res.send(mustache.render(templates.index, context, templates.partials));
     }).otherwise(function (err) {
         console.log(err);
         context.err = err;
-        context.things = [];
         res.send(mustache.render(templates.index, context, templates.partials));
     });
 });
@@ -32,14 +31,13 @@ app.get("/", function (req, res) {
 app.get("/things", function (req, res) {
     var response = {};
     var query = Object.assign({}, req.query);
-
     query.page = Number(query.page) || 1;
+    response.query = query;
+    response.prevPage = query.page - 1;
 
     viewster.getForQuery(query).then(function (result) {
         response.count = result.count || 0;
         response.total = result.total || 0;
-        response.query = query;
-        response.prevPage = query.page - 1;
         response.nextPage = (response.count - viewster.DEFAULT_PER_PAGE * query.page) > 0 ? query.page+ 1 : 0;
         var context = {
             things: result.things
@@ -48,9 +46,8 @@ app.get("/things", function (req, res) {
         res.json(response);
     }).otherwise(function (err) {
         console.log(err);
-        context.err = err;
-        context.things = [];
-        res.send(mustache.render(templates.index, context, templates.partials));
+        response.err = err;
+        res.json(response);
     });
 });
 

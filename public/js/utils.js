56520d653637fff5749043228937fb94146f4785
mblackstock <mikeblackstock@gmail.com>
2018-03-13 21:13:42 -0700

    fix #18 when typing show spinner and ‘Searching…’, delay only 500ms

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,8 +1,17 @@
 
 var utils = (function() {
-    function debounce(func, wait) {
+    function debounce(init, func, wait) {
         var timeout;
+        if (wait === undefined) {
+            wait = func;
+            func = init;
+            init = null;
+        }
+
         return function() {
+            if (!timeout && init) {
+                init();
+            }
             var context = this, args = arguments;
             var later = function() {
                 timeout = null;

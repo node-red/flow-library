d6add79b9af724aca697bbc6234bde2193134c9c
Nick O'Leary <nick.oleary@gmail.com>
2019-09-11 20:49:55 +0100

    Hide thingbox tools on mouseleave

--- public/js/utils.js
+++ public/js/utils.js
@@ -128,6 +128,7 @@ var utils = (function() {
                 thingList.find(".thing-list-nav-page-info").text(data.meta.pages.current+" of "+data.meta.pages.total).show();
             }
             list.html(data.html);
+            list.find(".gistbox-tools").on("mouseleave", function(e) { $(this).removeClass("open")})
             if (done) { done(data) }
         });
         return { then: function(d) { done = d } }

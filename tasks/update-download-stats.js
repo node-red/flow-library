cca95d63c39a3cb1f77641bc834f0c91f17718bc
Nick O'Leary <knolleary@users.noreply.github.com>
2020-11-11 14:11:11 +0000

    Add download stats action

--- tasks/update-download-stats.js
+++ tasks/update-download-stats.js
@@ -2,15 +2,7 @@ var settings = require("../config");
 var npmNodes = require("../lib/nodes");
 var npmModules = require("../lib/modules");
 
-npmModules.refreshDownloads().then(function(results) {
-    results.forEach(function(res) {
-        if (res.state === 'rejected') {
-            console.log("Failed:",res.reason);
-        } else if (res.value) {
-            console.log("Updated:",res.value);
-        }
-    });
-}).catch(function(err) {
+npmModules.refreshDownloads().then(() =>{}).catch(function(err) {
     console.log(err);
 }).then(function() {
     npmNodes.close();

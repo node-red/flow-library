1dc02c4f43a47477ef55f6f9aada9e05b8c4c18a
Nick O'Leary <knolleary@users.noreply.github.com>
2020-11-11 09:54:31 +0000

    Fix comparing old and new catalogues for changes

--- .github/scripts/compare-catalogs.js
+++ .github/scripts/compare-catalogs.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 var currentFile = JSON.parse(fs.readFileSync("catalogue.nodered.org/catalogue.json"))
 var newFile = JSON.parse(fs.readFileSync("catalogue.nodered.org/catalogue.json.new"))
 
-var currentModules = JSON.stringify(currentFile.moduels);
-var newModules = JSON.stringify(newFile.moduels);
+var currentModules = JSON.stringify(currentFile.modules);
+var newModules = JSON.stringify(newFile.modules);
 
 if (currentModules !== newModules) {
     process.exit(1)

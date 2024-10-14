3093c7d4eb3ee292ac7b480aa6021af485a763e4
Nick O'Leary <nick.oleary@gmail.com>
2024-10-14 09:52:12 +0100

    Update tasks for new mongo api

--- tasks/update-download-stats.js
+++ tasks/update-download-stats.js
@@ -1,9 +1,14 @@
-var settings = require("../config");
-var npmNodes = require("../lib/nodes");
-var npmModules = require("../lib/modules");
+const db = require('../lib/db')
+const npmModules = require('../lib/modules')
+const npmNodes = require('../lib/nodes')
 
-npmModules.refreshDownloads().then(() =>{}).catch(function(err) {
-    console.log(err);
-}).then(function() {
-    npmNodes.close();
-});
+;(async function () {
+    await db.init()
+    try {
+        await npmModules.refreshDownloads()
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await npmNodes.close()
+    }
+})()

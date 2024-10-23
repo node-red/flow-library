f374bc87345f4b68eab6f057c8d11d51d5d3c6db
Nick O'Leary <nick.oleary@gmail.com>
2024-10-23 13:41:00 +0100

    Fix pagination of search results

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const db = require('./lib/db')
 const templates = require('./lib/templates')
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 5 minutes
+    windowMs: 5 * 60 * 1000, // 5 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: false, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
